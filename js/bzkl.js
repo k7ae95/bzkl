9e1981faac220239555ef1d4f872647c995810ef
Noname <idonthave@email.com>
2017-04-21 23:06:50 +0700

    Update

--- js/bzkl.js
+++ js/bzkl.js
@@ -154,16 +154,18 @@
   }
 })(jQuery, window, document);
 
-function bodyOnload() {
-   var url = [
-      'https://7ydf13.blogspot.com/'
-   ];
-
-   $(url).each(function (index, value) {
-      parent.postMessage(document.body.scrollHeight, value);
-   });
-}
-
 $(document).ready(function () {
    $('.bzkl-chapter').bzkl();
+
+   $('body').load(function () {
+      var url = [
+         'https://7ydf13.blogspot.com'
+      ];
+
+      $(url).each(function (index, value) {
+         console.log(value);
+
+         parent.postMessage(document.body.scrollHeight, value);
+      });
+   });
 });
